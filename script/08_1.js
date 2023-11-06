192e243764e9bf555856efce2666d61545555db8
halloween96 <gudghdnls@naver.com>
2023-11-06 18:01:47 +0900

    주사위 게임 완성/ 업다운 게임 css

--- script/08_1.js
+++ script/08_1.js
@@ -1,13 +1,49 @@
+// 버튼처리
+const handleBtClick = (comImg, meImg, me, msg) => {
+// 1. 컴퓨터 랜덤수 생성
+//1 ~ 6까지
+    const com = Math.floor(Math.random() * 6) + 1;
+    console.log("com=", com, "me=", me);
+// 2. 주사위 이미지 변경
+    comImg.setAttribute("src", `./images/${com}.png`);
+    meImg.setAttribute("src", `./images/${me}.png`);
+// 3. 결과만들기
+    if (com === me) msg.innerHTML = "비겼습니다.";
+    else if (com > me) msg.innerHTML = "졌습니다.";
+    else msg.innerHTML = "이겼습니다.";
+}
 document.addEventListener("DOMContentLoaded", () => {
-    //1. 버튼 요소와 이미지 요소 가져오기
-    const bt = document.querySelector(`#msg1 > #bt${n}`);
-    const img = document.querySelector("#btDiv > img");
-    //2. 버튼 클릭 이벤트 처리하기
-    bt.addEventListener("click", () => {
-        //3. 랜덤수 생성하기
-        let n = Math.floor(Math.random() * 6) + 1;
-        console.log("n", n)
-        //4. 생성된 랜덤수에 해당하는 이미지 보여주기
-        img.setAttribute("src", `./images/${n}.png`);
-    });
+   //구성요소 찾기
+    const comImg = document.querySelector("#com");
+    const meImg = document.querySelector("#me");
+
+    const bts = document.querySelectorAll("#msg1 > button");
+    const msg = document.querySelector("#sec3 > h1");
+  
+    //기본 for 문장
+    // for(let i=0 ; i < bts.length ; i++) {
+    //     console.log(bts[i]);
+    // }
+
+    // for in 문장
+    // for(let i in bts) {
+    //     console.log(bts[i]);
+    // }
+
+    // for each 문장
+    // bts.forEach((item, idx) => {
+    //     console.log(item, idx)});
+
+    // for of 문장
+    //[구조분해]
+    // for(let [idx, item] of bts.entries()) {
+    //     console.log(idx, item);
+    // }
+    for(let bt of bts) {
+        bt.addEventListener("click", ()=>{
+            const me = parseInt(bt.getAttribute("id").slice(-1));
+            handleBtClick(comImg, meImg, me, msg);
+            console.log(bt.getAttribute("id").slice(-1));
+        })
+    }
 });
---
+++ script/09.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const txt = document.getElementById('txt');
+    txt.addEventListener('change', (e)=>{
+        console.log(e.target.value);
+    });
+})
